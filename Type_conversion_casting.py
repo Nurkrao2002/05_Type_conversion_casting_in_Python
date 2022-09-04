9f979b077f0640e0ee868eb3323d85bdc35b54e7
UDAY RAMA KOTESWA RAO NALABOTHU <111573702+uadaynalabothu@users.noreply.github.com>
2022-09-04 23:47:59 +0530

    Update Type_conversion_casting.py

--- Type_conversion_casting.py
+++ Type_conversion_casting.py
@@ -2,9 +2,9 @@
 
 """
 Note:-
-int --> %d or % %in
-float --> %f
-str --> %s
+int   -->  %d or % %in
+float -->  %f
+str   -->  %s
 """
 
 a=5; b=6.7 ; c='hello'
@@ -16,19 +16,23 @@ print(b)
 print('%f' %b)
 print('%.1f'%b)
 
+
 a=5
 print(float(a))
 print(str(a))
 print('%f' %a)
 print('%.1f' %a)
 
+
 b=6.8
 print(int(b))
 print(str(b))
 
+
 c='hello'
 print(int('c'))         # value error
 print(float(c))
+
 
 d='5'
 print(type(d))
@@ -37,6 +41,7 @@ print(e,type(e))
 f=float(d)
 print(f,type(f))
 
+
 g='4.7'
 print(type(g))
 print(int(g))            # ValueError
@@ -44,4 +49,5 @@ h=float(g)
 print(h,type(h))
 i=int(h)
 print(i,type(i))
+
 
