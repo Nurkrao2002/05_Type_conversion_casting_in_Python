97543958f08cfc40d073ce65ef64478dffda95af
UDAY RAMA KOTESWA RAO NALABOTHU <111573702+uadaynalabothu@users.noreply.github.com>
2022-09-05 20:13:09 +0530

    Update Input_Function.py

--- Input_Function.py
+++ Input_Function.py
@@ -1,24 +1,27 @@
 """input_functions"""
 
-# a=input('Enter your Name: ')
-# print(type(a))
 
-# b=int(input('Enter your Phone Number:'))
-# print(b,type(b))
+a=input('Enter your Name: ')
+print(type(a))
 
-# c=float(input('Enter your height:'))
-# print(c,type(c))
+b=int(input('Enter your Phone Number:'))
+print(b,type(b))
+
+c=float(input('Enter your height:'))
+print(c,type(c))
+
 
 """ Addition of two numbers """
-# a=float(input())
-# b=float(input())
-# print('The value of a+b is',a+b)
 
-# a,b=[float(x) for x in input().split()]
-# print(a+b)
+a=float(input())
+b=float(input())
+print('The value of a+b is',a+b)
 
-# a=1,23,34,35
-# print(a,type(a))
+a,b=[float(x) for x in input().split()]
+print(a+b)
 
-# a=[float(x) for x in input().split()]
-# print(a)
+a=1,23,34,35
+print(a,type(a))
+
+a=[float(x) for x in input().split()]
+print(a)
