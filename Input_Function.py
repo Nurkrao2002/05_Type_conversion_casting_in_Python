cbd2e9af4f2bbbd5ae870a5b0027cfd6f21cfcd9
UDAY RAMA KOTESWA RAO NALABOTHU <111573702+uadaynalabothu@users.noreply.github.com>
2022-09-05 20:11:51 +0530

    Add files via upload

---
+++ Input_Function.py
@@ -0,0 +1,24 @@
+"""input_functions"""
+
+# a=input('Enter your Name: ')
+# print(type(a))
+
+# b=int(input('Enter your Phone Number:'))
+# print(b,type(b))
+
+# c=float(input('Enter your height:'))
+# print(c,type(c))
+
+""" Addition of two numbers """
+# a=float(input())
+# b=float(input())
+# print('The value of a+b is',a+b)
+
+# a,b=[float(x) for x in input().split()]
+# print(a+b)
+
+# a=1,23,34,35
+# print(a,type(a))
+
+# a=[float(x) for x in input().split()]
+# print(a)
